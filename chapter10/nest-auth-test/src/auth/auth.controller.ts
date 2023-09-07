9ff8e64b169a81fae94223eb109485995a5c685b
의권 정 <jek9412@naver.com>
2023-09-07 18:35:32 +0900

    10.6.4 start

---
+++ chapter10/nest-auth-test/src/auth/auth.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Request,
+  Response,
+  UseGuards,
+  Get,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/user.dto';
+import { LoginGuard } from './auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() userDto: CreateUserDto) {
+    return await this.authService.register(userDto);
+  }
+
+  @Post('login')
+  async login(@Request() req, @Response() res) {
+    const userInfo = await this.authService.validateUser(
+      req.body.email,
+      req.body.password,
+    );
+
+    if (userInfo) {
+      res.cookie('login', JSON.stringify(userInfo), {
+        httpOnly: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      });
+    }
+    return res.send({ message: 'login success' });
+  }
+
+  @UseGuards(LoginGuard)
+  @Post('login2')
+  async login2(@Request() req, @Response() res) {
+    if (!req.cookies['login'] && req.user) {
+      res.cookie('login', JSON.stringify(req.user), {
+        httpOnly: true,
+        masAge: 1000 * 10,
+      });
+    }
+    return res.send({ message: 'login2 success' });
+  }
+
+  @UseGuards(LoginGuard)
+  @Get('test-guard')
+  testGuard() {
+    return '로그인될 때만 이 글이 보입니다.';
+  }
+}
